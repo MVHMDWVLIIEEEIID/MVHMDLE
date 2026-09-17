b013d5dde10c1028b2762265ce835e81895b0216
MVHMDWVLIIEEEIID <mvhmdwvliieeeiid@gmail.com>
2026-09-18 02:11:21 +0300

    removed some excessive stuff all workding good now

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { Routes, Route } from "react-router";
 import Menu from "./pages/Menu";
-import Lost from "./pages/Lost";
-import Setting from "./pages/Setting";
 import Daily from "./pages/Daily";
 import Survival from "./pages/Survival";
 import MobileBlocker from "./components/MobileBlocker"; // Import
@@ -13,8 +11,6 @@ export default function App() {
         <Route index element={<Menu />} />
         <Route path="/daily" element={<Daily />} />
         <Route path="/survival" element={<Survival />} />
-        <Route path="/Setting" element={<Setting />} />
-        <Route path="*" element={<Lost />} />
       </Routes>
     </MobileBlocker>
   );
